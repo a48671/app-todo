67cc472837280560b6082dbca392442cbce1a5c8
Andrey Fomin <a48671@gmail.com>
2019-02-11 20:16:06 +0300

    optimized sortTasks function

--- src/functions/sortTasks.js
+++ src/functions/sortTasks.js
@@ -1,24 +1,13 @@
 export const sortTasks = tasks => {
 
-    let arrayTitles = [];
-    tasks.forEach(task => {
-        const currentTitle = task.title;
-        if(currentTitle !== '') {
-            arrayTitles.push(currentTitle.toLowerCase());
-        }
-    });
-    arrayTitles.sort();
+    const sortedTasks = [...tasks];
 
-    let sortedTasks = [];
-    arrayTitles.forEach(title => {
-        tasks.forEach((task, i) => {
-            const lowCaseTitle = task.title.toLowerCase();
-            if(lowCaseTitle === title) {
-                sortedTasks.push(task);
-                tasks.splice(i, 1);
-            }
-        });
-    });
+    const compareFunction = (a, b) => {
+        if(a.title.toLowerCase() < b.title.toLowerCase()) return -1;
+        else return 1;
+    }
+
+    sortedTasks.sort(compareFunction);
 
     return sortedTasks;
 }
